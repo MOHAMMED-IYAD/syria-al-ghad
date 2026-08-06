8ef7099b18932926af03c80624d77d1633312cf6
MOHAMMED-IYAD <mohammedprograme@gmail.com>
2026-08-06 15:02:11 +0300

    update website

--- search.js
+++ search.js
@@ -67,6 +67,14 @@ const students = [
     },
 
     {
+        name: "رهف حبيبة محمد 😂😂😂😂",
+        grade: "الاول ",
+        section: "ب",
+        phone: "0599567890",
+        status: "منتظم"
+    },
+
+    {
         name: "مهدي القوقا ",
         grade: "الثاني ",
         section: "ب",
